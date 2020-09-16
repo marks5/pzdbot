d974b6815ada700aa2608c098fff2625e71b3ec9
Gabriel Marques <marques18@gmail.com>
2020-09-16 21:48:30 +0000

    Initial commit

---
+++ commands/args-info.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'args-info',
+  args: true,
+	description: 'Information about the arguments provided.',
+	execute(message, args) {
+		if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
+		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
+	},
+};
